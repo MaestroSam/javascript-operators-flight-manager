a64b7840ae7647f41a0f7d2179ae2992f3d191e1
MaestroSam <longssam@gmail.com>
2020-10-20 06:50:22 +0100

    Completed module Baby!

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,37 @@
 function Prices() {
+    function calculateFinalPrice(basePrice, passengerTypeDiscount, flightTypeDiscount) {
+
+        return finalPrice = basePrice + ((passengerTypeDiscount/100) * basePrice) + ((flightTypeDiscount/100) * (basePrice + (passengerTypeDiscount/100) * basePrice));
+    }
+
+    function calculateDefaultFinalPrice(basePrice, passengerType, flightType) {
+        passengerType = passengerType.toLowerCase();
+        flightType = flightType.toLowerCase();
+
+        //Discount rates
+        let regularPassenger = -5;
+        let vipPassenger = 5;
+        let economyFlight = -3;
+        let businessFlight = 10;
+
+        if (passengerType === "regular" && flightType === "economy") {
+            return defaultFinalPrice = basePrice + ((regularPassenger/100) * basePrice) + ((economyFlight/100) * (basePrice + (regularPassenger/100) * basePrice));
+        } else if (passengerType === "regular" && flightType === "business") {
+            return defaultFinalPrice = basePrice + ((regularPassenger/100) * basePrice) + ((businessFlight/100) * (basePrice + (regularPassenger/100) * basePrice));
+        } else if (passengerType === "vip" && flightType === "economy") {
+            return defaultFinalPrice = basePrice + ((vipPassenger/100) * basePrice) + ((economyFlight/100) * (basePrice + (vipPassenger/100) * basePrice));
+        } else if (passengerType === "vip" && flightType === "business") {
+            return defaultFinalPrice = basePrice + ((vipPassenger/100) * basePrice) + ((businessFlight/100) * (basePrice + (vipPassenger/100) * basePrice));
+        }
+    };
+
+    function calculateTotalFinalPrice(numOfSeats, passengerType, flightType, basePrice) {
+        return numOfSeats * calculateDefaultFinalPrice(basePrice, passengerType, flightType);
+    }
+
+    return { calculateFinalPrice, calculateDefaultFinalPrice, calculateTotalFinalPrice };
 
 }
+
+module.exports = Prices();
 
--- logic/util.js
+++ logic/util.js
@@ -17,7 +17,30 @@ function Util() {
     return totalNumberOfPassengers;
   }
 
-  return {
-    calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers };
-}
+  function checkInput(input) {
+    if (input != true) {
+      throw Error("Incorrect Input Entered: It must be a valid number");
+    }
+    return checkInput;
+  }
+
+  function calculateTotalDistance(distancesArray) {
+  //using .filter() o select only positive numbers then use .reduce() to sum values of distancesArray
+     let totalDistances = distancesArray.filter(item => item > 0).reduce((acc, curV) => { return acc + curV;}, 0);
+
+     return totalDistances;
+  }
+
+  function calculateBonusPoints(distTravelledByBusinessSeatsArray, distTravelledByEconomySeatsArray, businessBonus, economyBonus) {
+    let businessPoints = calculateTotalDistance(distTravelledByBusinessSeatsArray) * (businessBonus/100);
+
+    let economyPoints = calculateTotalDistance(distTravelledByEconomySeatsArray) * (economyBonus/100);
+
+    return totalBonus = businessPoints + economyPoints; 
+  }
+
+return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, calculateTotalDistance, calculateBonusPoints };
+
+  };
+
 module.exports = Util();
